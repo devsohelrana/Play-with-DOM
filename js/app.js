c971c1f6d1cbca67f83e9b9e406cab5037d57696
devsohelrana <dev.sohelrana@gmail.com>
2023-06-14 20:03:54 +0600

    Template File

---
+++ js/app.js
@@ -0,0 +1,32 @@
+/**
+* ===================================================
+* Title        : Analyze the DOM
+* Description  : Document Object Model Analyze
+* Author       : SOHEL RANA
+* Date         : 13-06-2023
+* ===================================================
+**/
+
+
+// console.dir(document);
+
+
+// document.title = 'This is new title';
+
+// console.log(document.title);
+
+// console.log(document.head);
+
+// console.log(document.body);
+
+// console.log(document.all);
+
+// for(var i of document.all) {
+//     console.log(i);
+// }
+
+// console.log(document.images);
+
+// console.log(typeof(document));
+
+
